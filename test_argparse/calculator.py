ef656f9da3294fb11d7145a1aa4ccd0516fcb564
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-11-30 18:03:33 +0330

    Delete test_argparse directory

--- test_argparse/calculator.py
+++
@@ -1,9 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--a",type=int,help="first number")
-parser.add_argument("--b",type=int,help="second number")
-args = parser.parse_args()
-a = args.a
-b = args.b
-result = a+b
-print(result)

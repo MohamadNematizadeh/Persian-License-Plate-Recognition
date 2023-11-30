b9665100493afcd8f943e2a390dcb0cc5a48bc70
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-11-30 17:57:15 +0330

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-from deep_text_recognition_benchmark.dtrb import DTRB
-
-# plate_detector = YOLO("weigths/yolov8-detector/yolov8-s-license-plate-detector.pt")  # load a pretrained model YOLOV8
-# plate_detector.predict("io/input/16_car.jpg",save=True,save_crop=True)
-
-plate_rec = DTRB("weigths/dtrb-recoginzer/dtrb-None-VGG-BiLSTM-CTC-license-plate-recognizer.pth")   # load a pretrained model DTRB
-plate_rec.predict("io/input_plates")

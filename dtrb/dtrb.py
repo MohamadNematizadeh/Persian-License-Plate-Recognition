a63ae8a38e23b65488cf94520b030c8f8099abbf
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-03 11:06:24 +0330

    Add file Persian_License_Plate_Recognition

--- dtrb/dtrb.py
+++ dtrb/dtrb.py
@@ -1,20 +1,22 @@
 import string
 import argparse
-
 import torch
 import torch.backends.cudnn as cudnn
 import torch.utils.data
 import torch.nn.functional as F
+import torchvision.transforms as transforms
 
-from dtrb.utils import CTCLabelConverter, AttnLabelConverter
-from dtrb.dataset import RawDataset, AlignCollate
-from dtrb.model import Model
+
+from deep_text_recognition_benchmark.utils import CTCLabelConverter, AttnLabelConverter
+from deep_text_recognition_benchmark.dataset import RawDataset, AlignCollate
+from deep_text_recognition_benchmark.model import Model
+
 
 parser = argparse.ArgumentParser()
-#parser.add_argument('--image_folder', required=True, help='path to image_folder which contains text images')
+# parser.add_argument('--image_folder', required=True, help='path to image_folder which contains text images')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)
 parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
-#parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
+# parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
 """ Data processing """
 parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
 parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
@@ -24,10 +26,10 @@ parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnop
 parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
 parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
 """ Model Architecture """
-parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
-parser.add_argument('--FeatureExtraction', type=str, required=True, help='FeatureExtraction stage. VGG|RCNN|ResNet')
-parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
-parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
+parser.add_argument('--Transformation', type=str, default="TPS", help='Transformation stage. None|TPS')
+parser.add_argument('--FeatureExtraction', type=str, default="ResNet", help='FeatureExtraction stage. VGG|RCNN|ResNet')
+parser.add_argument('--SequenceModeling', type=str, default="BiLSTM", help='SequenceModeling stage. None|BiLSTM')
+parser.add_argument('--Prediction', type=str, default="Attn", help='Prediction stage. CTC|Attn')
 parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
 parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
 parser.add_argument('--output_channel', type=int, default=512,
@@ -35,17 +37,17 @@ parser.add_argument('--output_channel', type=int, default=512,
 parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
 
 opt = parser.parse_args()
-if opt.sensitive:
-    opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
-
-cudnn.benchmark = True
-cudnn.deterministic = True
-opt.num_gpu = torch.cuda.device_count()
-
-
 
 class DTRB:
-    def __init__(self,weights_path):
+    def __init__(self, weights_path):
+        if opt.sensitive:
+            opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
+
+        cudnn.benchmark = True
+        cudnn.deterministic = True
+        opt.num_gpu = torch.cuda.device_count()
+
+        """ model configuration """
         if 'CTC' in opt.Prediction:
             self.converter = CTCLabelConverter(opt.character)
         else:
@@ -54,82 +56,98 @@ class DTRB:
 
         if opt.rgb:
             opt.input_channel = 3
-
+        
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.load_model(weights_path)
-        
 
-    def load_model(self,weights_path):
+    def load_model(self, weights_path):
         self.model = Model(opt)
         print('model input parameters', opt.imgH, opt.imgW, opt.num_fiducial, opt.input_channel, opt.output_channel,
             opt.hidden_size, opt.num_class, opt.batch_max_length, opt.Transformation, opt.FeatureExtraction,
             opt.SequenceModeling, opt.Prediction)
-        model = torch.nn.DataParallel(self.model).to(self.device)
+        self.model = torch.nn.DataParallel(self.model).to(self.device)
 
         # load model
         print('loading pretrained model from %s' % weights_path)
-        model.load_state_dict(torch.load(weights_path, map_location=self.device))
+        self.model.load_state_dict(torch.load(weights_path, map_location=self.device))
 
-    def load_data(self,imag_folder):
+    def load_data(self, image_folder):
+        # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
         AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
-        demo_data = RawDataset(root=imag_folder, opt=opt)  # use RawDataset
+        demo_data = RawDataset(root=image_folder, opt=opt)  # use RawDataset
         self.demo_loader = torch.utils.data.DataLoader(
-        demo_data, batch_size=opt.batch_size,
-        shuffle=False,
-        num_workers=int(opt.workers),
-        collate_fn=AlignCollate_demo, pin_memory=True)
-        
-    def predict(self,imag_folder):
-        self.load_data(imag_folder)
+            demo_data, batch_size=opt.batch_size,
+            shuffle=False,
+            num_workers=int(opt.workers),
+            collate_fn=AlignCollate_demo, pin_memory=True)
+
+    def predict(self, image):
+        AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            # transforms.Normalize(0, 1)
+            ])
+            
+        # predict
         self.model.eval()
         with torch.no_grad():
-            for image_tensors, image_path_list in self.demo_loader:
-                batch_size = image_tensors.size(0)
-                image = image_tensors.to(self.device)
-                # For max length prediction
-                length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(self.device)
-                text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(self.device)
+            image_tensor = transform(image)
+            image_tensor = image_tensor.sub_(0.5).div_(0.5)
+            image_tensor = torch.unsqueeze(image_tensor, 0) # output: [1, 1, 32, 100]
+            print(image_tensor.shape)
+            # image_tensor = torch.permute(image_tensor, (0, 1, 3, 2))
+            # print(image_tensor.shape)
 
-                if 'CTC' in opt.Prediction:
-                    preds = self.model(image, text_for_pred)
+            # image_tensor = AlignCollate_demo(image_tensor)
+            
+            # for image_tensors, image_path_list in self.demo_loader:
+            batch_size = image_tensor.size(0)
+            image = image_tensor.to(self.device)
+            # For max length prediction
+            length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(self.device)
+            text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(self.device)
 
-                    # Select max probabilty (greedy decoding) then decode index to character
-                    preds_size = torch.IntTensor([preds.size(1)] * batch_size)
-                    _, preds_index = preds.max(2)
-                    # preds_index = preds_index.view(-1)
-                    preds_str = self.converter.decode(preds_index, preds_size)
+            if 'CTC' in opt.Prediction:
+                preds = self.model(image, text_for_pred)
 
-                else:
-                    preds = self.model(image, text_for_pred, is_train=False)
+                # Select max probabilty (greedy decoding) then decode index to character
+                preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+                _, preds_index = preds.max(2)
+                # preds_index = preds_index.view(-1)
+                preds_str = self.converter.decode(preds_index, preds_size)
 
-                    # select max probabilty (greedy decoding) then decode index to character
-                    _, preds_index = preds.max(2)
-                    preds_str = self.converter.decode(preds_index, length_for_pred)
+            else:
+                preds = self.model(image, text_for_pred, is_train=False)
+
+                # select max probabilty (greedy decoding) then decode index to character
+                _, preds_index = preds.max(2)
+                preds_str = self.converter.decode(preds_index, length_for_pred)
 
 
-                log = open(f'./log_demo_result.txt', 'a')
-                dashed_line = '-' * 80
-                head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'
-                
-                print(f'{dashed_line}\n{head}\n{dashed_line}')
-                log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
+            log = open(f'./log_demo_result.txt', 'a')
+            dashed_line = '-' * 80
+            head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'
+            
+            print(f'{dashed_line}\n{head}\n{dashed_line}')
+            log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
 
-                preds_prob = F.softmax(preds, dim=2)
-                preds_max_prob, _ = preds_prob.max(dim=2)
-                for img_name, pred, pred_max_prob in zip(image_path_list, preds_str, preds_max_prob):
-                    if 'Attn' in opt.Prediction:
-                        pred_EOS = pred.find('[s]')
-                        pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
-                        pred_max_prob = pred_max_prob[:pred_EOS]
+            preds_prob = F.softmax(preds, dim=2)
+            preds_max_prob, _ = preds_prob.max(dim=2)
+            for img_name, pred, pred_max_prob in zip(["besco"], preds_str, preds_max_prob):
+                if 'Attn' in opt.Prediction:
+                    pred_EOS = pred.find('[s]')
+                    pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                    pred_max_prob = pred_max_prob[:pred_EOS]
 
-                    # calculate confidence score (= multiply of pred_max_prob)
-                    confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+                # calculate confidence score (= multiply of pred_max_prob)
+                confidence_score = pred_max_prob.cumprod(dim=0)[-1]
 
-                    print(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}')
-                    log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
+                print(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}')
+                log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
 
-                log.close()
+            log.close()
+
 
 if __name__ == "__main__":
-    plate_rec =dtrb("../weigths/dtrb-recoginzer/best_accuracy.pth")
-    plate_rec.predict("../io/input_plates")
+    plate_recognizer = DTRB("../weigths/dtrb-recoginzer/dtrb-None-VGG-BiLSTM-CTC-license-plate-recognizer.pth")
+    plate_recognizer.predict("../io/input_plates")

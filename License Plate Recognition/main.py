83b200eae0f14348f0d76b51e2e5d9581282d490
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-04 13:16:16 +0330

    Delete License Plate Recognition directory

--- License Plate Recognition/main.py
+++
@@ -1,6 +0,0 @@
-import cv2
-from ultralytics import YOLO
-from deep_text_recognition_benchmark.dtrb import DTRB
-
-plate_recognizer = DTRB("weigths/dtrb-recoginzer/dtrb-None-VGG-BiLSTM-CTC-license-plate-recognizer.pth")   # load a pretrained model DTRB
-plate_recognizer.predict("io/input")

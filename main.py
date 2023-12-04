5ce40f1726e960c78ab1ebcd3ffde96d219ee081
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-04 14:54:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ results = plate_detector.predict(image)
 for result in results:
     for i in range(len(result.boxes.xyxy)):
         if result.boxes.conf[i] > 0.6:
-            bbox_tensor = result.boxes.xyxy[i]
-            bbox_ndarray = bbox_tensor.cpu().detach().numpy().astype(int)
-            print(bbox_ndarray)
-            x1, y1, x2, y2 = bbox_ndarray
+            bbox = result.boxes.xyxy[i]
+            bbox = bbox.cpu().detach().numpy().astype(int)
+            print(bbox)
+            x1, y1, x2, y2 = bbox
             plate_image = image[y1:y2, x1:x2].copy()
-
-            cv2.imwrite(f"io/output/plake_image_result_{i}.jpg", plate_image)
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 4)
-            
             plate_image = cv2.resize(plate_image, (100, 32))
             plate_image = cv2.cvtColor(plate_image, cv2.COLOR_BGR2GRAY)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 4)
             plate_recognizer.predict(plate_image)
 
 cv2.imwrite("io/output/image_result_2.jpg", image)

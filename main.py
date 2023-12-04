d0efc9763c825d01ab332630d4e57abc6057cfaa
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-04 13:34:05 +0330

    Add-V3

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+from ultralytics import YOLO
+from deep_text_recognition_benchmark.dtrb import DTRB
+
+plate_detector = YOLO("weigths/yolov8-detector/yolov8-s-license-plate-detector.pt")
+plate_recognizer = DTRB("weigths/dtrb-recoginzer/dtrb-None-VGG-BiLSTM-CTC-license-plate-recognizer.pth")
+
+image = cv2.imread("io/input/2.bmp")
+results = plate_detector.predict(image)
+for result in results:
+    for i in range(len(result.boxes.xyxy)):
+        if result.boxes.conf[i] > 0.6:
+            bbox_tensor = result.boxes.xyxy[i]
+            bbox_ndarray = bbox_tensor.cpu().detach().numpy().astype(int)
+            print(bbox_ndarray)
+            x1, y1, x2, y2 = bbox_ndarray
+            plate_image = image[y1:y2, x1:x2].copy()
+
+            cv2.imwrite(f"io/output/plake_image_result_{i}.jpg", plate_image)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 4)
+            
+            plate_image = cv2.resize(plate_image, (100, 32))
+            plate_image = cv2.cvtColor(plate_image, cv2.COLOR_BGR2GRAY)
+            plate_recognizer.predict(plate_image)
+
+cv2.imwrite("io/output/image_result_2.jpg", image)

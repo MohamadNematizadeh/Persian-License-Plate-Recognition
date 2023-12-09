2d81c0e225dbd98c1a82a9c0ab8d76d0ca97c811
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-09 13:20:55 +0330

    Update Creating_data.py

--- Creating_data.py
+++ Creating_data.py
@@ -1,12 +1,12 @@
 import sqlite3 
-from difflib import SequenceMatcher
 
 class Database:
     def __init__(self):
         self.con = sqlite3.connect('db/license_plate_recognition.db')
         self.cursor = self.con.cursor()
 
-    def get_Plake_text(self , text_plake_labl):
-        self.cursor.execute(f"SELECT * FROM Plake WHERE text = '{text_plake_labl}'")
-        plak = self.cursor.fetchone()
-        return plak
+    def get_Plake_text(self):
+        query="SELECT * FROM Plake"
+        result=self.cursor.execute(query)
+        plates=result.fetchall()
+        return plates
